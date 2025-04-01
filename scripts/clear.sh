17ddc05a3aeb7e64a01a71a81436c3afd43a51e6
CircuitMurderer <1020333562@qq.com>
2025-04-01 21:27:59 +0800

    fix clear.sh

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -rf ./par_data/* ./seq_data/* ./temp/* ./results/* ./*.csv ./*.json
+rm -rf ./par_data/* ./seq_data/* ./temp/* ./results/* ./*.csv ./*.hdf ./*.json
 rm -rf ./ckks/*.bin ./ckks/*.csv ./ckks/*.txt ./ckks/vbuff/*
